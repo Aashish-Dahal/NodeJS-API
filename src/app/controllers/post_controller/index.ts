c369d87abf88164c2190b5424188cae9427ebd37
Aashish-Dahal <ashishdahal490@gmail.com>
2023-01-02 19:07:29 +0545

    fix: createPost controller

--- src/app/controllers/post_controller/index.ts
+++ src/app/controllers/post_controller/index.ts
@@ -1,10 +1,18 @@
 import { asyncHandler } from "../../middleware/async";
 import { NextFunction, Request, Response } from "express";
-import { firestore } from "firebase-admin";
+import { userPost } from "../../../config/db";
 
 export const createPost = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     const user = req["currentUser"];
+    const { message } = req.body;
+    await userPost.doc(user.user_id).set({
+      message: message,
+    });
+    res.status(200).json({
+      success: true,
+      user: "post created successfully",
+    });
   }
 );
 export const deletePost = asyncHandler(
@@ -14,5 +22,13 @@ export const editPost = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {}
 );
 export const posts = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {}
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = req["currentUser"];
+    const posts = await userPost.doc(user.user_id).get();
+
+    res.status(200).json({
+      success: true,
+      post: posts.data(),
+    });
+  }
 );
