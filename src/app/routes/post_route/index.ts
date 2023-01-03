c54734bba4ac809fd9ad9f73339e9f060dece46e
Aashish-Dahal <ashishdahal490@gmail.com>
2023-01-03 15:13:24 +0545

    feat: update,delete route

--- src/app/routes/post_route/index.ts
+++ src/app/routes/post_route/index.ts
@@ -1,7 +1,18 @@
 import { Router } from "express";
-import { createPost, posts } from "../../controllers/post_controller";
+import {
+  createPost,
+  deletePost,
+  posts,
+  updatePost,
+  userPosts,
+} from "../../controllers/post_controller";
 import { protect } from "../../middleware/auth";
 const router = Router();
+
 router.post("/create", protect, createPost);
-router.get("/get", protect, posts);
+router.get("/posts", protect, posts);
+router.get("/user", protect, userPosts);
+router.put("/update/:id", protect, updatePost);
+router.delete("/delete/:id", protect, deletePost);
+
 export default router;
