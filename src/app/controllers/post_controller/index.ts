520721b24cb54e25bfcb196fa42e5e359e7998a0
Aashish-Dahal <ashishdahal490@gmail.com>
2023-01-03 15:11:54 +0545

    feat: post update,delete controller

--- src/app/controllers/post_controller/index.ts
+++ src/app/controllers/post_controller/index.ts
@@ -6,29 +6,66 @@ export const createPost = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     const user = req["currentUser"];
     const { message } = req.body;
-    await userPost.doc(user.user_id).set({
+    const id = userPost.doc().id;
+    await userPost.doc(id).set({
+      id: id,
       message: message,
+      user: user.user_id,
     });
     res.status(200).json({
       success: true,
-      user: "post created successfully",
+      message: "post created successfully",
     });
   }
 );
 export const deletePost = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {}
+  async (req: Request, res: Response, next: NextFunction) => {
+    await userPost.doc(req.params.id).delete();
+    res.status(200).json({
+      success: true,
+      message: "Deleted successfully",
+    });
+  }
 );
-export const editPost = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {}
+export const updatePost = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { message } = req.body;
+    console.log(req.params.id);
+    await userPost.doc(req.params.id).update({
+      message: message,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Updated successfully",
+    });
+  }
 );
 export const posts = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    const user = req["currentUser"];
-    const posts = await userPost.doc(user.user_id).get();
+    let allPosts: any[] = [];
+    const posts = await userPost.get();
 
+    posts.forEach((doc: any) => {
+      allPosts.push(doc.data());
+    });
     res.status(200).json({
       success: true,
-      post: posts.data(),
+      data: allPosts,
+    });
+  }
+);
+export const userPosts = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = req["currentUser"];
+    let allPosts: any[] = [];
+    const posts = await userPost.where("user", "==", user.user_id).get();
+
+    posts.forEach((doc: any) => {
+      allPosts.push(doc.data());
+    });
+    res.status(200).json({
+      success: true,
+      data: allPosts,
     });
   }
 );
